e4bd4f5c696449db4e6dc4cd642d55bf49c0e1c7
ArtemFilosof <afilosof1@gmail.com>
2023-09-20 22:18:49 +0300

    Expl007 Works right!!!

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -1,52 +1,45 @@
 ﻿//Треугольник Серпинского
-// using System;
-// using System.Threading;
-// using Internal;
-// //using Internal;
 
 Console.Clear();
-Console.SetCursorPosition(10,4);
-Console.WriteLine("+");
 
-int xa = 1;
-int ya = 1;
-int xb = 1;
-int yb = 30;
-int xc = 80;
-int yc = 30;
-//Console.SetCursorPosition(1,1);
+int xa = 40; int ya = 1;
+int xb = 1; int yb = 20;
+int xc = 80; int yc = 20;
+
 Console.SetCursorPosition(xa, ya);
-Console.WriteLine("+");
+Console.WriteLine("A");
 
 Console.SetCursorPosition(xb, yb);
-Console.WriteLine("+");
+Console.WriteLine("B");
 
 Console.SetCursorPosition(xc, yc);
-Console.WriteLine("+");
+Console.WriteLine("C");
 
-Console.SetCursorPosition(1,1);
 int count = 0;
-int x =xa, y = ya;
+int x = xa, y = ya;
 while (count < 10000)
 {
-    int what = new Random().Next(0, 3);//(0;3) 0 1 2
+    int what = new Random().Next(0, 3);//[0;3) 0 1 2
 
     if (what == 0)
     {
-        x = (x + ya) / 2;
+        x = (x + xa) / 2;
         y = (y + ya) / 2;
     }
     if (what == 1)
     {
-        x = (x + yb) / 2;
+        x = (x + xb) / 2;
         y = (y + yb) / 2;
     }
     if (what == 2)
     {
-        x = (x + yc) / 2;
+        x = (x + xc) / 2;
         y = (y + yc) / 2;
     }
-    Console.SetCursorPosition(x,y);
-    Console.WriteLine("+");
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("┼");
     count++;
 }
+Console.SetCursorPosition(0, 21);
+
+Console.WriteLine("END");
